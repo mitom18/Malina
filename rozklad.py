ad9e67a41f48964dde47761a8cf217438f42e336
Michal Toman <tomanm@ag309-2.arcig>
2017-11-28 16:56:34 +0100

    prvni python

---
+++ rozklad.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+from math import sqrt
+cislo=input("Zadejte číslo, které mám otestovat:")
+while(cislo>1):
+  odm=sqrt(cislo)+1 #kvůli chybám v odmocnině
+  d=2
+  while(cislo%d>0)and(d<=odm):
+    d+=1
+  if(d>odm):
+    d=cislo
+  print(d)
+  cislo/=d
+
