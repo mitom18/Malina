6ff6b2698e738f2ea96cb24ab9fb18346aed2dd5
Radian-Fi <34257022+Radian-Fi@users.noreply.github.com>
2018-04-24 16:23:14 +0200

    Added DC motor support (motor.py), release candidate

--- playDice.py
+++ playDice.py
@@ -3,6 +3,17 @@ import time
 import cv2
 import numpy as np
 #from matplotlib import pyplot as plt
+import RPi.GPIO as gpio
+from time import sleep
+
+gpio.setmode (gpio.BOARD)
+
+gpio.setup (11, gpio.OUT, initial = gpio.LOW)
+sleep (1)
+gpio.output (11, gpio.HIGH)
+sleep (2)
+gpio.output (11, gpio.LOW)
+gpio.cleanup()
 
 camera = picamera.PiCamera()
 
